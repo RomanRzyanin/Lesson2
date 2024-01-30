91b7f2bc5a04d33b9a2dba51c5870995ddf3c070
Roman Rzyanin <roman@rzyanin.ru>
2024-01-30 23:13:35 +0300

    lesson2:task2,task3,task4

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//int n = 10;
+int[] arr = { 2, 6, 4, 12, 8, 36, 8, 14, 8, 0 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i++;
+}
---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] arr = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = arr[0];
+
+while (i < arr.Length)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i++;
+}
+Console.WriteLine(max);
